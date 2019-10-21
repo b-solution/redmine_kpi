bf083241ffa66401342ed53e92aa41f3c7250f2d
bilel-kedidi <bilel.kedidi@gmail.com>
2019-10-21 16:02:08 +0100

    fix

--- lib/redmine_kpi/time_entry_patch.rb
+++ lib/redmine_kpi/time_entry_patch.rb
@@ -10,7 +10,7 @@ module  RedmineKpi
        safe_attributes 'origin_issue_id'
         before_save do
           approval_custom_field = Setting.plugin_redmine_kpi['approval_custom_field']
-          if approval_custom_field.present? && (cf = TimeEntryCustomField.find_by(approval_custom_field)) && ( custom_field_value = self.visible_custom_field_values.detect{|cfv| cfv.custom_field_id == cf.id} )
+          if approval_custom_field.present? && (cf = TimeEntryCustomField.find_by(id: approval_custom_field)) && ( custom_field_value = self.visible_custom_field_values.detect{|cfv| cfv.custom_field_id == cf.id} )
             custom_field_value.value = false unless User.current.allowed_to_globally?(:approve_time_entry, {})
           end
         end
